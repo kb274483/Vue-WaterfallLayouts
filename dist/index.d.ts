bf579a8ea0bf36cc7d3d61a49022ac7dfa19ea13
Roy <roy@gastom.com.tw>
2025-09-05 15:09:32 +0800

    update:gitignore and pack name

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import type { App } from 'vue';
-import WaterfallContainer from '@/components/WaterfallContainer.vue';
-export { WaterfallContainer };
-declare const _default: {
-    install(app: App): void;
-};
-export default _default;
-export * from '@/types';
-export * from '@/composables/useWaterfall';
